e2685ace39c2e452a8347f49077c6f9f1c02b53c
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-06 10:03:43 +0500

    better

--- Slicing_list08.py
+++ Slicing_list08.py
@@ -1,4 +1,4 @@
-def main(list1,n):
+def main(list1, n):
     """
     A list of several elements is given. Return all items from end n steps.
     Args:
@@ -7,4 +7,9 @@ def main(list1,n):
     Returns:
         list: return answer.
     """
-    return
+    list1.reverse()
+    return list1[::n]
+
+
+v = main(['a', 1, 'b', 2, 'c', 3, 'd', 4], 2)
+print(v)
